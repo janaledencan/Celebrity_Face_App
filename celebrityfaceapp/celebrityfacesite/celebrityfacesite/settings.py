c691e7ce5505073f37ca3f0fdf49e6d197eafaa2
janaledencan <janaledencan@gmail.com>
2024-01-23 23:22:05 +0100

    Create and activate model and add Django Admin

--- celebrityfaceapp/celebrityfacesite/celebrityfacesite/settings.py
+++ celebrityfaceapp/celebrityfacesite/celebrityfacesite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "faceapp.apps.FaceappConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- celebrityfaceapp/celebrityfacesite/faceapp/models.py
+++ celebrityfaceapp/celebrityfacesite/faceapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Sex(models.TextChoices):
+    male = "M"
+    female = "F"
+
+
+class User(models.Model):
+    age = models.PositiveIntegerField()
+    sex = models.CharField(
+        max_length = 1,
+        choices = Sex.choices,
+        default = Sex.male,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.age} - {self.sex}"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+

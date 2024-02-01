f352f6fbe65c4c7943547039d52cb701940e2c53
janaledencan <janaledencan@gmail.com>
2024-02-01 13:21:40 +0100

    Add html templates and change urls.py and views.py

--- celebrityfaceapp/celebrityfacesite/celebrityfacesite/urls.py
+++ celebrityfaceapp/celebrityfacesite/celebrityfacesite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path 
 
+from faceapp import views
+
 urlpatterns = [
     path("faceapp/", include("faceapp.urls")),
     path("admin/", admin.site.urls),
--- celebrityfaceapp/celebrityfacesite/faceapp/views.py
+++ celebrityfaceapp/celebrityfacesite/faceapp/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the faceapp index.")
+def home(request):
+    context = {}
+    return render(request, "faceapp/home.html", context)
